7b214559d0531eb302173d68504ed88539bff868
Stoyann Velten <stoyann.openclassrooms@gmail.com>
2023-08-22 12:12:04 +1100

    table contact fixed #5

--- models/Contact.js
+++ models/Contact.js
@@ -44,6 +44,27 @@ const ContactSchema = new mongoose.Schema(
     function: {
       type: String,
     },
+
+    address: {
+      type: String,
+      
+    },
+
+    location: {
+      type: {
+        type: String,
+        enum: ["Point"],
+      },
+      coordinates: {
+        type: [Number],
+        index: "2dsphere",
+      },
+      formattedAddress: String,
+      street: String,
+      city: String,
+      zipcode: String,
+      country: String,
+    },
   },
 
   {
@@ -52,5 +73,27 @@ const ContactSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   }
 );
+
+// Geocode & create location
+ContactSchema.pre("save", async function (next) {
+  if (this.address) {
+    const loc = await geocoder.geocode(this.address);
+    this.location = {
+      type: "Point",
+      coordinates: [loc[0].longitude, loc[0].latitude],
+      formattedAddress: loc[0].formattedAddress,
+      street: loc[0].streetName,
+      city: loc[0].city,
+      state: loc[0].stateCode,
+      zipcode: loc[0].zipcode,
+      country: loc[0].countryCode,
+    };
+
+    // Do not save address
+    this.address = undefined;
+  }
+  next();
+});
+
 
 module.exports = mongoose.model("Contact", ContactSchema);
